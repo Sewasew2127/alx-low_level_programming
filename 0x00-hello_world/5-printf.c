19315cc81f110890867a75e29c46ee97855714ed
Sewasew <emo21.sew@gmail.com>
2022-04-07 05:43:55 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+ * main - prints string using prinf
+ * return - 0returns 0 if success
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
