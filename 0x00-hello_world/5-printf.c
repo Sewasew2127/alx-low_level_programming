0b913240e002b6ae4878dc25298cd35559b3629f
Sewasew <emo21.sew@gmail.com>
2022-04-07 05:45:30 -0700

    why betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-/*
+/**
  * main - prints string using prinf
  * return - 0returns 0 if success
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
