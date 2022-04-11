d99bc9d2f39bfc5ad02193520296c12e70a87a40
Sewasew <emo21.sew@gmail.com>
2022-04-11 04:15:54 -0700

    two taks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 'z'; x >= 'a'; --x)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
