4b1d95bd746b4f3ca76ee1f82a22b7b9fe25682d
Sewasew <emo21.sew@gmail.com>
2022-04-11 02:17:22 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all the letters except p and e
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; ++c)
+	{
+		if (c != 4 && c != 18)
+			putchar('a' + c);
+
+	}
+	putchar('\n');
+	return (0);
+}
