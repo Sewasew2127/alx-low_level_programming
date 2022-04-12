6bd5fc0fa189a109052ba5f15a6bd84cb1b088dc
Sewasew <emo21.sew@gmail.com>
2022-04-12 05:22:55 -0700

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
 			if (i == 98)
+			{
 				printf("%d", i);
+				break;
+			}
+			printf("%d, ", i);
 		}
 	}
 	else if(n == 98)
@@ -27,9 +30,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
 			if (i == 98)
+			{
 				printf("%d", i);
+				break;
+			}
+			printf("%d, ", i);
 		}
 	}
 	printf("\n");
