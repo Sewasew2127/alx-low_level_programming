1f28d4e4a413ee0cba33b96446d06dd942e72427
Sewasew <emo21.sew@gmail.com>
2022-04-12 23:58:46 -0700

    advanced task 1

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that computes and prints the sum of all the
+ *  multiples of 3 or 5 below 1024 (excluded)
+ *
+ * Return: 0i
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("\n");
+}
