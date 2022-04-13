99f50aa5fb0fe0ae3361489f62641461d12a1464
Sewasew <emo21.sew@gmail.com>
2022-04-13 00:03:31 -0700

    simple mistake

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,16 @@
 int main(void)
 {
 	int i;
+	int sum;
 
+	sum = 0;
 	for (i = 1; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("%d, ", i);
+			sum = sum + i;
 		}
 	}
-	printf("\n");
+	printf("%d \n", sum);
 	return (0);
 }
