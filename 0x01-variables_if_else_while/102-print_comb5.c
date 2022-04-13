8cb422f1c2d48fc00e2b61eb93a2a633410b4ff9
Sewasew <emo21.sew@gmail.com>
2022-04-13 01:11:30 -0700

    resubmit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 		for (j1 = 0; j1 < 10; j1++)
 		{
-			for (i2 = i1; i1 < 10; i2++)
+			for (i2 = i1; i2 < 10; i2++)
 			{
-				for (j2= ((i1 == i2) ? (j1 + 1) : 0); j2 < 10; ++j2)
+				for (j2 = ((i1 == i2) ? (j1 + 1) : 0); j2 < 10; ++j2)
 				{
 					putchar('0' + i1);
 					putchar('0' + j1);
