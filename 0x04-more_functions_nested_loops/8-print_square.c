215b380765980ff2022194b2e2dae4c3dc66f2d7
Sewasew <emo21.sew@gmail.com>
2022-04-14 06:11:31 -0700

    squares and fizzbuzz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: the size of the square
+ *
+ * Return: void.
+ */
+void print_square(int size)
+{
+	int row;
+	int col;
+
+	for (col = 0; col <= size; col++)
+	{
+		for (row = 0; row <= size; row++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Fizz buzz
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		if (n < 100)
+			putchar(' ');
+		else
+			putchar('\n');
+	}
+	return (0);
+}
