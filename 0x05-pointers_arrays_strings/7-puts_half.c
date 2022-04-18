11683cc24732b6ee00ac4deda3df2baa736b00f7
Sewasew <emo21.sew@gmail.com>
2022-04-18 06:14:29 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,14 @@ void puts_half(char *str)
 	}
 	if (i % 2 != 0)
 	{
-		for (j = (i - 1) / 2; j <= i; j++)
+		for (j = (i - 1) / 2; j < i; j++)
 		{
 			_putchar(str[j]);
 		}
 	}
 	else
 	{
-		for (j = i / 2; j <= i; j++)
+		for (j = i / 2; j < i; j++)
 		{
 			_putchar(str[j]);
 		}
