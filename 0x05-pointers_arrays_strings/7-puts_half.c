3d2b7c061e7138eb7fdeaa9017e35d04dcaa94b3
Sewasew <emo21.sew@gmail.com>
2022-04-18 06:43:55 -0700

    tasks

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 void puts_half(char *str)
 {
 	int i;
-	
+
 	if (str)
 	{
 		for (i = 0; *(str + i); ++i)
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string
+ * @dest: buffer pointed
+ * @src: src of the string
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	if (dest && strc)
+	{
+		while (*(src + i))
+			++i;
+		if (src + i < dest || dest + i < src)
+		{
+			do {
+				*(dest + j++) = *src++;
+			} while (i--)
+		}
+	}
+	return (dest);
+}
