cea727ecb4067e00e1ee6dae403317eb6699ef8b
Sewasew <emo21.sew@gmail.com>
2022-04-18 02:00:14 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes apointer to an int,
+ * and updates its value to 98
+ * @n: a pointer the interger we want to set to 98
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
