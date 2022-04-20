30c085562c575556b272b7b99d5ca62fe110e574
Sewasew <emo21.sew@gmail.com>
2022-04-20 04:01:21 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ *
+ * Description: a function that works like strcmp
+ *
+ * Retrun: integer.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 && *s2; ++s1, ++s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+	}
+	if (*s1)
+		return (1);
+	if (*s2)
+		return (-1);
+	return (0);
+}
