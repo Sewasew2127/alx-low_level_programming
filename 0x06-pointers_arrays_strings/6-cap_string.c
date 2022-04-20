19a7521a3eb74c45110a16412eb474ab78320184
Sewasew <emo21.sew@gmail.com>
2022-04-20 06:33:36 -0700

    tasks

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: capitalized string.
+ */
+char *cap_string(char *s)
+{
+	char *separator = " \t\n,;.!?\"(){}";
+	int i;
+	int j;
+
+	for (i =0, j = 0; s[i]; ++i)
+	{
+		if ('a' <= s[i] && s[i] <= 'z')
+		{
+			if (i)
+			{
+				for (j = 0; ws[j] && ws[j] != s[i - 1]; ++j)
+					;
+			}
+		}
+		if (separator[j])
+			s[i] -= ('a' - 'A');
+	}
+	return (s);
+}
+
