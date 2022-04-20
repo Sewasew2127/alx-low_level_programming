957d64996aed299718fe456361182da3e30dc905
Sewasew <emo21.sew@gmail.com>
2022-04-20 01:02:53 -0700

    string concatination with out the c libarary

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: the resulting string
+ * @src: the src string
+ *
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte
+ *
+ * Return: a pointer to the destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	/* store the length of src in i */
+	for (i = 0; src[i] != '\0'; ++i)
+		;
+	/* concatenate src to dest */
+	for (j = 0; src[j] != '\0'; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
