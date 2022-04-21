a25c2728532f8f57ef4ce8803a8c34eed56aa31e
Sewasew <emo21.sew@gmail.com>
2022-04-20 23:59:45 -0700

    advanced task 2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - print number
+ * @n: the integer to be printed
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	if (n > -1)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+}
