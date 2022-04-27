2128171ba0fb9d7cf2a8d516a0f5ae23018312ed
Sewasew <emo21.sew@gmail.com>
2022-04-27 01:52:49 -0700

    task 6 - is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _help_prime - helper for the net function
+ * @x: the integer to be checkde
+ * @y: the divisor
+ *
+ * Return: 1 if x is prime, otherwise 0.
+ */
+int _help_prime(int x, int y)
+{
+	if (x % y == 0)
+		return (0);
+	if (x < y * 2)
+		return (1);
+	return(_help_prime(x, y + 1));
+}
+/**
+ * is_prime_number - a function returns 1 if n is prime number
+ * @n: the integer to be checkd
+ *
+ * Return: 1 if true otherwise 0.
+ */
+int is_prime_number(int n)
+{
+	if ( n < 4)
+		return (n > 1);
+	return (_help_prime(n, 2));
+}
