94d055165b23526adcc7e7b8d995eaffcb994754
Sewasew <emo21.sew@gmail.com>
2022-04-26 23:31:33 -0700

    project 0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - a funcion that sets the value of apointer to a char.
+ * @s: a pointer to pointer
+ * @to: a pointer.
+ *
+ * Return: Nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
