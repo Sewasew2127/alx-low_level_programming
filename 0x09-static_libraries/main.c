eaeb495f87657c3f1343241ab83b0b872802cbde
sewasew <emo21.sew@gmail.com>
2022-05-04 14:40:05 +0300

    fixing betty may reslove my 23th check

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #include<stdio.h>
-
+/**
+ * main - entry point
+ *
+ * Return: Nothing
+ **/
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
+- Kevin Mitnick");
+return (0);
 }
