d403eb090467af05bd1069f0409aeffc47dba03a
sewasew <emo21.sew@gmail.com>
2022-05-12 11:34:28 +0300

    Function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef FUNCTION-POINTERS_H
-#define FUNCTION-POINTERS_H	
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H	
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
