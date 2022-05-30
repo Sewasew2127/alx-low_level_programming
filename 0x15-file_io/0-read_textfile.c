36cf38d270f84f70d7d7aa068a14b9e40c934338
Sewasew <emo21.sew@gmail.com>
2022-05-30 02:38:14 -0700

    task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/*
+ * read_textfile - reads a text file and prints it to the POSIX stadard outout
+ * @filename: path of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters it could read and print if succes
+ * else if not opend or read return 0
+ * else if the filename is NULL return 0
+ * otherwise 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer = NULL;
+	ssize_t to_read;
+	ssize_t written;
+	inf fd;
+
+	if(!(filename && letters))
+		return (0);
+	
+	fd = open(filename, O_RDONLY);
+	
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if(!buffer)
+		return (0);
+
+	to_read = read(fd, buffer, letters);
+	close(fd);
+
+	if (to_read < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	if (!to_read)
+		to_read = letters;
+	written = write(STDOUT_FILENO, buffer, b_read);
+	free(buffer);
+
+	if(written < 0)
+		return (0);
+
+	return (written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,17 @@
+# File I/O
+
+## What is the difference between system call and function call?
+	-> the differece is :- System call is a request for the kernel to access a resource while function call is a request made by a program to perform a specfic task.
+			  :- System calls are used when a program needs to communciate with the kernel while function calls are used to call a specfic function within a program
+
+=======================================================================================================
+	
+	# Files in the directory
+
+* 0-read_textfile.c - a function that reads a text file and prints it to the POSIX standard output
+
+* 1-create_file.c - a function that creates a file
+
+* 2-append_text_to_file.c - a function that appends text at the end of a file
+
+* 3-cp.c - a function that copies the content of a file to another file
